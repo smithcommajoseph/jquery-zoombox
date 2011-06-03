43afbce116fe65c83e90fc8e516e78465e6c4763
Joseph Smith <technicolorenvy@gmail.com>
2011-06-03 15:45:23 -0600

    flail flail flail

--- jquery.zoombox.js
+++ jquery.zoombox.js
@@ -13,22 +13,46 @@
     _binds = function(params, $trigger){
         $trigger.bind('click.zoomboxEvents', function(e){
             e.preventDefault();
-            
+
             var o = _calcGrowPoint(e, params),
                 $zc = $('#'+params.containerId),
                 startmap = {'left': o.x+'px', 'top': o.y+'px'},
                 animapLeft = (params.targetPosX !== undefined) ? params.targetPosX : o.x - parseInt(params.targetWidth / 2, 10),
                 animapTop = (params.targetPosY !== undefined) ? params.targetPosY : o.y - parseInt(params.targetHeight / 2, 10),
-                animap = {'left': animapLeft+'px',width: params.targetWidth,'top': animapTop+'px',height: params.targetHeight};
+                animapGrow = {'left': animapLeft+'px', width: params.targetWidth, 'top': animapTop+'px', height: params.targetHeight},
+                animapShrink = {'left': animapLeft+'px', 'top': animapTop+'px', 'width': '1px', 'height': '1px'};
+            
+            console.log('o');
+            console.log(o);
+            console.log('params.targetWidth');
+            console.log(params.targetWidth);
+            console.log('params.targetHeight');
+            console.log(params.targetHeight);
+            console.log('startmap');
+            console.log(startmap);
+            console.log('animapLeft');
+            console.log(animapLeft);
+            console.log('animapTop');
+            console.log(animapTop);
+            console.log('animapShrink');
+            console.log(animapShrink); 
+
             
             $zc.css(startmap);
             
             if($zc.hasClass('inactive')){
-                $zc.css('opacity', '1').animate(animap, params.zoomboxAnimationSpeed, params.zoomboxEasing, function(){
-                    $zc.addClass('active');
+                $zc.css('opacity', '1').animate(animapGrow, params.zoomboxAnimationSpeed, params.zoomboxEasing, function(){
+                    $zc.removeClass('inactive').addClass('active');
                 });
+                console.log('inside zc.hasclass active is TRUE');
+                console.log(animapGrow);
             } else {
-
+                $zc.animate(animapShrink, params.zoomboxAnimationSpeed, params.zoomboxEasing, function(){
+                    $zc.removeClass('active').addClass('inactive');
+                });
+                console.log('inside zc.hasclass active is FALSE');
+                console.log(animapShrink);
+                
             }
         });
     },
@@ -44,12 +68,23 @@
     _calcGrowPoint = function(e, params){
         var o = {};
         
-        if(params.growFromMouse) { o.x = e.pageX; o.y = e.pageY; }
-        else { 
-            var offset = $(e.target).offset();
-            o.x = offset.left; 
-            o.y = offset.top;
+        if($(this).data('zoomcalcs') === undefined) {
+                        
+            if(params.growFromMouse) { o.x = e.pageX; o.y = e.pageY; }
+            // some stubbed stuff here for optional growth from a set point, would need to set context here as well for classes
+            // may not actually get implemented
+            // else if (params.growFromPoint !== undefined ){ o.x = params.growFromPoint; o.y = params.growFromPoint } 
+            else { 
+                var offset = $(e.currentTarget).position();
+                o.x = offset.left; 
+                o.y = offset.top;
+            }
+            
+            $(this).data('zoomcalcs', o);
+        } else {
+            o = $(this).data('zoomcalcs');
         }
+        
         return o;
     },
     
@@ -62,10 +97,9 @@
             return this.each(function(){
                 var $trigger = $(this),
                     params = $.extend($.fn.zoombox.defaults, options),
-                    containerParent = (params.containerParent !== undefined) ? params.containerParent : 'body',
                     $div = $('<div id="'+params.containerId+'" class="inactive"/>').css({'background-color': 'green',  'opacity': '0', 'width': '1px', 'height': '1px', 'position': 'absolute'});
                     
-                $(containerParent).append($div);
+                $(params.containerParent).append($div);
                 
                 _binds(params, $trigger);
                 
@@ -77,12 +111,16 @@
                 
             });
         },
+        
         close: function(){
             return this.each(function(){
-                console.log('inside close return');
+                var $trigger = $(this),
+                params = $.extend($.fn.zoombox.defaults, options),
+                $zc = $('#'+params.containerId);
                 
             });
         },
+        
         destroy: function(){
             return this.each(function(){
                 console.log('inside destroy return');
@@ -106,11 +144,11 @@
     
     $.fn.zoombox.defaults = {
         containerId:                "zoombox-container",
-        containerParent:            undefined,
+        containerParent:            'body',
         closeWhenEsc:               true,
         closeWhenSelfIsNotClicked:  true,
         growFromMouse:              true,
-        growFromPoint:              'the click selector or somesuch',
+        growFromPoint:              undefined,
         showCloseBtn:               true,
         targetHeight:               '200',
         targetWidth:                '400',
