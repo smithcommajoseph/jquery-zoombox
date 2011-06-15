7d4cc4a76b02a2f4b66a162755c21875f23d917d
Joseph Smith <technicolorenvy@gmail.com>
2011-06-15 16:39:02 -0600

    building out tests

--- test/unit/jquery.zoombox.js
+++ test/unit/jquery.zoombox.js
@@ -1,20 +1,20 @@
 (function($){
 	//some vars
-	var xYCoords = '230, 230',
+	var xYCoords = [230, 230],
 	$anchor = $('<a />').attr({
 								href: 'json_test.html',
 								id: 'test-anchor',
-								rel: xYCoords
+								rel: xYCoords.join(', ')
 								}).html('Anchor'),
 	$button = $('<button />').attr({
 								id: 'test-button',
 								type: 'submit',
-								zB: xYCoords
+								zB: xYCoords.join(',')
 								}).html('Button'),
 	$submit = $('<input />').attr({
 								id: 'test-submit',
 								type: 'submit',
-								zB: xYCoords
+								zB: xYCoords.join(',')
 								}).html('Input type=submit'),
 	zoomboxAnchorSetup = function(){
 		$('body').append($anchor);
@@ -30,13 +30,13 @@
 		
 		test('Defaults ob and props', function(){
 			
-			expect(19);
+			expect(21);
 			
 			//does the defaults object exist
 			equals(typeof $.fn.zoombox.defaults, 'object', '$.fn.zoombox.defaults should be an object');
 			
 			//how many defaults are there?
-			var expected = 17,
+			var expected = 19,
 				actual = 0,
 				prop;
 				
@@ -58,6 +58,8 @@
 			equals(typeof $.fn.zoombox.defaults.growFromTagAttr, 'boolean', '$.fn.zoombox.defaults.growFromTagAttr should exist and be typeof "boolean"');
 			equals(typeof $.fn.zoombox.defaults.growTagAttr, 'undefined', '$.fn.zoombox.defaults.growTagAttr should exist and be typeof "undefined"');
 			equals(typeof $.fn.zoombox.defaults.openCallback, 'object', '$.fn.zoombox.defaults.openCallback should exist and be typeof "object"');
+			equals(typeof $.fn.zoombox.defaults.preOpen, 'object', '$.fn.zoombox.defaults.openCallback should exist and be typeof "object"');
+			equals(typeof $.fn.zoombox.defaults.preClose, 'object', '$.fn.zoombox.defaults.openCallback should exist and be typeof "object"');
 			equals(typeof $.fn.zoombox.defaults.targetHeight, 'string', '$.fn.zoombox.defaults.targetHeight should exist and be typeof "string"');
 			equals(typeof $.fn.zoombox.defaults.targetWidth, 'string', '$.fn.zoombox.defaults.targetWidth should exist and be typeof "string"');
 			equals(typeof $.fn.zoombox.defaults.targetPosX, 'undefined', '$.fn.zoombox.defaults.targetPosX should exist and be typeof "undefined"');
@@ -66,7 +68,7 @@
 			equals(typeof $.fn.zoombox.defaults.zoomboxAnimationSpeed, 'string', '$.fn.zoombox.defaults.zoomboxAnimationSpeed should exist and be typeof "string"');
 		});
 
-		module('Zoombox Functionality (Anchor)', {setup: zoomboxAnchorSetup, teardown: zoomboxAnchorTeardown});
+		module('Functionality (Anchor)', {setup: zoomboxAnchorSetup, teardown: zoomboxAnchorTeardown});
 		
 		test('Basic Setup (Pre-zoomed)', function(){
 			
@@ -88,20 +90,83 @@
 			
 			stop();
 			
-			$('#test-anchor').zoombox({openCallback: function(){
-				equals(typeof $('#zoombox-container'), 'object', 'Zoombox container should exist in the dom');
-				equals($('#'+$.fn.zoombox.defaults.containerId).css('position'), 'absolute', 'Zoombox container position should be "absolute"');
-				equals($('#'+$.fn.zoombox.defaults.containerId).css('width'), $.fn.zoombox.defaults.targetWidth+'px', 'Zoombox container width should be "'+$.fn.zoombox.defaults.targetWidth+'px"');
-				equals($('#'+$.fn.zoombox.defaults.containerId).css('height'), $.fn.zoombox.defaults.targetHeight+'px', 'Zoombox container height should be "'+$.fn.zoombox.defaults.targetHeight+'px"');
-				equals($('#'+$.fn.zoombox.defaults.containerId).css('opacity'), '1', 'Zoombox container opacity should be "1"');
-				equals($('#'+$.fn.zoombox.defaults.containerId).hasClass('active'), true, 'Zoombox container should have the class "active"');
+			$('#test-anchor').zoombox({
+				openCallback: function(){
+					equals(typeof $('#zoombox-container'), 'object', 'Zoombox container should exist in the dom');
+					equals($('#'+$.fn.zoombox.defaults.containerId).css('position'), 'absolute', 'Zoombox container position should be "absolute"');
+					equals($('#'+$.fn.zoombox.defaults.containerId).css('width'), $.fn.zoombox.defaults.targetWidth+'px', 'Zoombox container width should be "'+$.fn.zoombox.defaults.targetWidth+'px"');
+					equals($('#'+$.fn.zoombox.defaults.containerId).css('height'), $.fn.zoombox.defaults.targetHeight+'px', 'Zoombox container height should be "'+$.fn.zoombox.defaults.targetHeight+'px"');
+					equals($('#'+$.fn.zoombox.defaults.containerId).css('opacity'), '1', 'Zoombox container opacity should be "1"');
+					equals($('#'+$.fn.zoombox.defaults.containerId).hasClass('active'), true, 'Zoombox container should have the class "active"');
 				
-				start();
-			}});
+					start();
+				}
+			});
 			
 			$('#test-anchor').click();
 			
 		});
 		
+		test('preOpen Setup', function(){
+			
+			expect(1);
+			
+			stop();
+			
+			$('#test-anchor').zoombox({
+				preOpen: function(){
+					$('#zoombox-container').append('<div id="test-div" />');
+				},
+				openCallback: function(){
+					equals(typeof $('#zoombox-container #test-div'), 'object', 'Test-Div container should exist in the dom');
+					start();
+				}
+			});
+			
+			$('#test-anchor').click();
+			
+		});
+		
+		test('preClose Setup', function(){
+			
+			expect(1);
+			
+			stop();
+			
+			$('#test-anchor').zoombox({
+				preClose: function(){
+					$('#zoombox-container').append('<div id="test-div" />');
+				},
+				closeCallback: function(){
+					equals(typeof $('#zoombox-container #test-div'), 'object', 'Test-Div container should exist in the dom');
+					start();
+				}
+			});
+			
+			$('#test-anchor').click();
+			
+		});
+		
+		// test('relTag Setup', function(){
+		// 	
+		// 	expect(2);
+		// 	
+		// 	stop();
+		// 	
+		// 	$('#test-anchor').zoombox({
+		// 		growFromTagAttr: true,
+		// 		growTagAttr: 'rel',
+		// 		closeCallback: function(){
+		// 			equals(parseInt($('#zoombox-container').css('left'), 10), xYCoords[0], '#zoombox-container should = first rel tag val');
+		// 			equals(parseInt($('#zoombox-container').css('top'), 10), xYCoords[1], '#zoombox-container should = second rel tag val');
+		// 			
+		// 			start();
+		// 		}
+		// 	});
+		// 	
+		// 	$('#test-anchor').click();
+		// 	
+		// });
+		
 	});
-})(jQuery)
+})(jQuery);
