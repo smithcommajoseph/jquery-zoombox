932a09dd7d72a49ca464325b79c55fc070950caa
Joseph Smith <technicolorenvy@gmail.com>
2011-06-28 21:40:16 -0600

    building out examples

--- examples/js/examples.js
+++ examples/js/examples.js
@@ -3,11 +3,16 @@
 	$(function(){
 		$('#example-1').zoombox();
 		
-		$('.example-2').zoombox({
-			containerId: '#zoombox-ex-2-container'
-			// preOpen: function(){
-			// 	$('#zoombox-ex-2-container').append('<p>Some Content here</p>');
-			// }
+		$('#example-2').zoombox({
+			preOpen: function(e){
+				$($(e.currentTarget).data('zoomboxTarget')).append('<p>Some Content here</p>');
+			}
+		});
+		
+		$('#example-3').zoombox({
+			openCallback	: function(e){
+				$($(e.currentTarget).data('zoomboxTarget')).append('<p>Some stuff here</p>');
+			}
 		});
 
 	});
