88cca6532531bc1d83348d3c127c37aa0ffcc68c
Joseph Smith <technicolorenvy@gmail.com>
2011-07-02 16:22:25 -0600

    building out examples

--- examples/js/examples.js
+++ examples/js/examples.js
@@ -10,11 +10,52 @@
 		});
 		
 		$('#example-3').zoombox({
-			openCallback	: function(e){
-				$($(e.currentTarget).data('zoomboxTarget')).append('<p>Some stuff here</p>');
+			preOpen: function(e){
+				$($(e.currentTarget).data('zoomboxTarget')).append('<p style="display:none;">I was hidden but now i\'m visible</p>');
+			},
+			openCallback: function(e){
+				$($(e.currentTarget).data('zoomboxTarget')).find('p').fadeIn();
 			}
 		});
-
+		
+		$('#example-4').zoombox({
+			preOpen: function(e){
+				$($(e.currentTarget).data('zoomboxTarget')).append('<p style="display:none;">I was lost but now i\'m found, and then i\'ll be lost again</p>');
+			},
+			openCallback: function(e){
+				$($(e.currentTarget).data('zoomboxTarget')).find('p').fadeIn();
+			},
+			preClose: function(){
+				$('.zoombox-container').find('p').remove();
+			}
+		});
+		
+		$('#example-5').zoombox({
+			closeCallback: function(){
+				alert('close callback fn fired');
+			}
+		});
+		
+		$('#example-6').zoombox({
+			targetWidth: '100',
+			targetHeight: '400'
+		});
+		
+		$('#example-7').zoombox({
+			targetPosX: '300',
+			targetPosY: '0',
+			containerParent: '#ex-7-offset-parent'
+		});
+		
+		$('#example-8').zoombox({
+			growFromMouse: true
+		});
+		
+		$('#ex-9-offset-parent a').zoombox({
+			growTagAttr: 'rel',
+			containerParent: '#ex-9-offset-parent'
+		});
+		
 	});
 	
 	
